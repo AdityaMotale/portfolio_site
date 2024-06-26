b501f1227c217b9a15b688458e506ee290a36111
Adii <77727680+AdityaMotale@users.noreply.github.com>
2024-06-26 23:30:00 +0530

    fixed footer overflow and added contact section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,24 +14,44 @@
 	</header>
 	<slot />
 	<footer
-		class="w-full flex md:flex-row flex-col md:items-start md:gap-0 gap-12 items-center justify-between px-[10%] mt-44 mb-24"
+		class="w-full flex md:flex-row flex-col md:items-start md:gap-0 gap-12 items-center
+		justify-between md:px-[20%] px-[10%] mt-44 mb-24"
 	>
 		<div class="flex items-center justify-start font-display font-medium text-lg gap-4">
 			<p>© 2024 Aditya Motale</p>
 			<p class="font-semibold text-xl">•</p>
 			<p>Bharat</p>
 		</div>
-		<div class="flex flex-col md:items-start items-center gap-8 font-display text-base font-medium">
-			<!-- Title -->
 
-			<p class="font-semibold text-lg">Elsewhere</p>
+		<div class="flex md:flex-row flex-col md:items-start items-center justify-start gap-20">
+			<div
+				class="flex flex-col md:items-start items-center gap-8 font-display text-base font-medium"
+			>
+				<!-- Title -->
 
-			<!-- Contacts -->
+				<p class="font-semibold text-lg">Elsewhere</p>
 
-			<a href="https://github.com/AdityaMotale">GitHub</a>
-			<a href="https://www.linkedin.com/in/aditya-motale/">LinkedIn</a>
-			<a href="https://www.figma.com/@adityamotale">Figma</a>
-			<a href="https://read.cv/adii">CV</a>
+				<!-- Contacts -->
+
+				<a href="https://github.com/AdityaMotale">GitHub</a>
+				<a href="https://www.linkedin.com/in/aditya-motale/">LinkedIn</a>
+				<a href="https://www.figma.com/@adityamotale">Figma</a>
+				<a href="https://read.cv/adii">CV</a>
+			</div>
+
+			<div
+				class="flex flex-col md:items-start items-center gap-8 font-display text-base font-medium"
+			>
+				<!-- Title -->
+
+				<p class="font-semibold text-lg">Contact</p>
+
+				<!-- Contacts -->
+
+				<a href="mailto:adityamotale.22@gmail.com">EMail</a>
+				<a href="https://mail.google.com/mail/u/0/?fs=1&to=adityamotale.22@gmail.com&tf=cm">GMail</a
+				>
+			</div>
 		</div>
 	</footer>
 </main>
